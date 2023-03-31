976db67178dc010309ea7a7f680f685d594eb15a
Mathias <mathias.vanhecke@gmail.com>
2023-03-31 14:32:12 +0200

    additional ItemControllerIntegrationTest

--- src/test/java/com/switchfully/eurder/item/api/ItemControllerIntegrationTest.java
+++ src/test/java/com/switchfully/eurder/item/api/ItemControllerIntegrationTest.java
@@ -4,12 +4,15 @@ import com.switchfully.eurder.item.domain.repositories.ItemRepository;
 import com.switchfully.eurder.item.service.dtos.ItemDTO;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,7 +29,7 @@ class ItemControllerIntegrationTest {
 
     @Test
     void whenIPostAnItem_thenTheRepositoryContainsThisItem() {
-        ItemDTO newItemDTO = new ItemDTO("itemName","someDescription",100.0,1);
+        ItemDTO newItemDTO = new ItemDTO("itemName", "someDescription", 100.0, 1);
         RestAssured
                 .given()
                 .contentType(ContentType.JSON)
@@ -41,4 +44,27 @@ class ItemControllerIntegrationTest {
         assertTrue(itemController.getAllItems().contains(newItemDTO));
     }
 
+    @Test
+    void whenTheRepositoryContains2Customers_thenICanRetrieveThemViaTheAPI() {
+        ItemDTO newItemDTO1 = new ItemDTO("itemName", "someDescription", 100.0, 1);
+        ItemDTO newItemDTO2 = new ItemDTO("anotherItemName", "someDescriptionAgain", 35.35, 35);
+        itemController.addItem(newItemDTO1);
+        itemController.addItem(newItemDTO2);
+
+        List<ItemDTO> list = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .when()
+                .port(port)
+                .get("/items") // http://localhost:???/contacts
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .body()
+                .jsonPath()
+                .getList(".", ItemDTO.class);
+
+        Assertions.assertThat(list).containsExactlyInAnyOrder(newItemDTO1, newItemDTO2);
+    }
 }
